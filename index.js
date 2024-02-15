12b6054181e78a0012921e1efd6330b58b9f9042
jwaddell10 <115955410+jwaddell10@users.noreply.github.com>
2024-02-15 13:42:33 -0600

    will navigate to the correct path

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 const http = require('http');
 const url = require('url');
 const fs = require('fs');
-
-
+const myURL = 'http://localhost:8080';
 
 http.createServer(function (req, res) {
-    fs.readFile('index.html', 'utf8', (err, data) => {
+    const q = url.parse(req.url, true);
+    const filename = {
+        '/': 'index.html',
+        '/about': 'about.html',
+        '/contact-me': 'contact-me.html'
+    }
+    console.log(filename[q.pathname], 'this is filename')
+    fs.readFile(filename[q.pathname], 'utf8', (err, data) => {
         if (err) {
           res.writeHead(404, {'Content-Type': 'text/html'})
           return res.end('404 not found')
@@ -14,5 +20,5 @@ http.createServer(function (req, res) {
         res.write(data);
         return res.end();
       });
-   }).listen(8080, 'localhost');
+   }).listen(8080);
    console.log('Server running at http://localhost:8080');
