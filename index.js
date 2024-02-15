677a82e03d86f3206e88bc18757cd57c27e0f056
jwaddell10 <115955410+jwaddell10@users.noreply.github.com>
2024-02-15 14:10:38 -0600

    navigate to diff files, without url.parse

--- index.js
+++ index.js
@@ -1,24 +1,25 @@
-const http = require('http');
-const url = require('url');
-const fs = require('fs');
-const myURL = 'http://localhost:8080';
+const http = require("http");
+const url = require("node:url");
+const fs = require("fs");
 
-http.createServer(function (req, res) {
-    const q = url.parse(req.url, true);
-    const filename = {
-        '/': 'index.html',
-        '/about': 'about.html',
-        '/contact-me': 'contact-me.html'
-    }
-    console.log(filename[q.pathname], 'this is filename')
-    fs.readFile(filename[q.pathname], 'utf8', (err, data) => {
-        if (err) {
-          res.writeHead(404, {'Content-Type': 'text/html'})
-          return res.end('404 not found')
-        }
-        res.writeHead(200, {'Content-Type': 'text/html'})
-        res.write(data);
-        return res.end();
-      });
-   }).listen(8080);
-   console.log('Server running at http://localhost:8080');
+http
+  .createServer(function (req, res) {
+    const myURL = new URL("http://localhost:8080" + req.url);
+    console.log(myURL.pathname, "this is myurlpath");
+    const pathways = {
+      "/": "index.html",
+      "/about": "about.html",
+      "/contact-me": "contact-me.html",
+    };
+    fs.readFile(pathways[myURL.pathname], "utf8", (err, data) => {
+      if (err) {
+        res.writeHead(404, { "Content-Type": "text/html" });
+        return res.end("404 not found");
+      }
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.write(data);
+      return res.end();
+    });
+  })
+  .listen(8080);
+console.log("Server running at http://localhost:8080");
